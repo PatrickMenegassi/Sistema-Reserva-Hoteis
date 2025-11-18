fc1115e8816456a865f28728f4db4315ef98af8b
Patrick <pmwalter@inf.ufpel.edu.br>
2025-11-18 15:03:55 -0300

    Correções

--- reservahotel/settings.py
+++ reservahotel/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!!)d)nj%=u#4fy03nlg-j6scf!n$k07hb$b4f0&w08t!7pz&-%'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-temporaria-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
